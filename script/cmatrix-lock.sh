576844ada167ad0452927305d762324a8df7adfd
Mini-Desktop <x1594076192@outlook.com>
2025-10-27 13:17:32 +0800

    用cmatrix实现锁屏

---
+++ script/cmatrix-lock.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# 启动 kitty 终端运行 cmatrix
+fcitx5-remote -c
+kitty --start-as fullscreen --title cmatrix -e bash -c "sleep 0.2; cmatrix -b -u 5 "&
+
+# cmatrix 运行完或者锁屏解锁后再恢复
+# fcitx5-remote -o
+
+pid=$!  # 保存 kitty 进程 PID
+
+# 等待 kitty 窗口出现
+# while true; do
+#     win_id=$(xdotool search --name "cmatrix" | tail -1)
+#     [ -n "$win_id" ] && break
+#     sleep 0.1
+# done
+
+# 使用 i3-msg 将窗口切换为全屏
+# i3-msg "[id=$win_id] fullscreen enable"
+
+# 启动锁屏程序
+i3lock -n -c 00000000
+
+# 解锁后关闭 kitty 窗口
+kill "$pid"
+
